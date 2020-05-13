c7a625d3a9d8a0083883a8bc4fe21db95d8cd885
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-13 09:03:21 +0900

    일곱번째 프로잭트올립니다

---
+++ ysg/src/ysg/Ex03_02.java
@@ -0,0 +1,17 @@
+package ysg;
+
+public class Ex03_02 {
+
+	public static void main(String[] args) {
+		int a;
+		float b;
+		
+		a = (int) 123.45f;
+		b = 200;
+		
+		System.out.printf("a의 값 ==> %d \n", a);
+		System.out.printf("b의 값 ==> %f \n", b);
+
+	}
+
+}
