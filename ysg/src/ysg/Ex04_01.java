565fcdf8387310454dffd8edd7d9d7dbcafe80cb
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-13 10:52:18 +0900

    여덟번째 프로잭트올립니다

---
+++ ysg/src/ysg/Ex04_01.java
@@ -0,0 +1,16 @@
+package ysg;
+
+public class Ex04_01 {
+
+	public static void main(String[] args) {
+		int a =10, b;
+		
+		b = a++;
+		System.out.printf("%d \n", b);
+		
+		b = ++a;
+		System.out.printf("%d \n", b);
+
+	}
+
+}
