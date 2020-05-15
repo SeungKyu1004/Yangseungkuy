c4885c8ad25385c007414f7bfc46cd2a7fab7af6
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-15 11:00:55 +0900

    실급 7_1올립니다.

---
+++ ysg/src/ysg/Ex07_01.java
@@ -0,0 +1,16 @@
+package ysg;
+
+public class Ex07_01 {
+
+	public static void main(String[] args) {
+		int i;
+		i = 0;
+		
+		while (i <5) {
+			System.out.printf("while 문을 공부합니다. \n");
+			i++;
+		}
+
+	}
+
+}
