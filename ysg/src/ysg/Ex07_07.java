2d2add4b940883ac859c34730957d8c7abff4a57
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-22 11:00:06 +0900

    실습 7_7올립니다.

---
+++ ysg/src/ysg/Ex07_07.java
@@ -0,0 +1,19 @@
+package ysg;
+
+public class Ex07_07 {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for(i=1; i<=100; i++) {
+			System.out.printf("for 문을 %d 회 실행했습니다.\n", i);
+			if(i==57) {
+				System.out.printf("for 문을 종료했습니다. \n");
+				break;
+		
+	    }
+
+    }
+}
+	}
+
