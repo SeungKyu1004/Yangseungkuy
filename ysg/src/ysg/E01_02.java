a9853c8f7a2b0c5c41b9abb17a417f583b9e3a06
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-26 17:20:18 +0900

    연습 01_02 올립니다.

--- ysg/src/ysg/E01_02.java
+++ ysg/src/ysg/E01_02.java
@@ -2,13 +2,10 @@ package ysg;
 
 public class E01_02 {
 
-	final static double PI = 3.141592;
-	
 	public static void main(String[] args) {
-		
-		int r =30;
-		System.out.println(r * r * PI);
-
+		for(int i =0; i<100; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
