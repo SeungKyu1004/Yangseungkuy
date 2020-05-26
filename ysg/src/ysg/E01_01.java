1708b764b919c9711cae31ebdbdcc5601ff91ac4
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-26 17:10:42 +0900

    연습01_01 올립니다.

---
+++ ysg/src/ysg/E01_01.java
@@ -0,0 +1,16 @@
+package ysg;
+
+public class E01_01 {
+
+	public static void main(String[] args) {
+		
+		int intType = 100;
+		double doubleType = 150.5;
+		String stringType = "¾ç½Â±Ô";
+		
+		System.out.println(intType);
+		System.out.println(doubleType);
+		System.out.println(stringType);
+	}
+
+}
