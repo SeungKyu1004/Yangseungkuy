70a3fafd328d76328a1eb33761f109798167c2de
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-27 16:51:11 +0900

    연습 1_4올립니다.

---
+++ ysg/src/ysg/E01_04.java
@@ -0,0 +1,22 @@
+package ysg;
+
+public class E01_04 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int evenSum = 0;
+		int oddSum = 0;
+		
+		while(i <= 100) {
+			if(i % 2 == 0) {
+				evenSum += i;
+			}else if(i % 2 ==1) {
+				oddSum += i;
+			}
+			i++;
+		}
+		System.out.println("Â¦¼öÀÇ ÇÕ : " + evenSum);
+		System.out.println("È¦¼öÀÇ ÇÕ : " + oddSum);
+	}
+
+}
