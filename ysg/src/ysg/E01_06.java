56f79b41bc8a74761e34835d97854a4c996627f9
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-28 13:39:26 +0900

    연습 E01_06 올립니다.

---
+++ ysg/src/ysg/E01_06.java
@@ -0,0 +1,18 @@
+package ysg;
+
+public class E01_06 {
+
+	public static void main(String[] args) {
+		int hap = 0;
+		int i;
+		
+		i=1;
+		while(i<=10) {
+			hap = hap + i;
+			i++;
+		}
+
+		System.out.printf(" 1에서 10까지의 합 : %d \n", hap);
+	}
+
+}
