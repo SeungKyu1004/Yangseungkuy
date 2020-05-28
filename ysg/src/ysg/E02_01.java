55523ec492ac7a7a91768935ecb16ad353c0242f
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-28 13:34:26 +0900

    연습 2_1 올립니다.

---
+++ ysg/src/ysg/E02_01.java
@@ -0,0 +1,17 @@
+package ysg;
+
+import java.util.Scanner;
+
+public class E02_01 {
+
+	public static void main(String[] args) {
+		int i;
+		i = 0;
+		
+		while (1<5) {
+			System.out.printf("while 문을 공격합니다. \n");
+			i++;
+		}
+	}
+
+}
