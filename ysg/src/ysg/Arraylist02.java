3c58d2a47a9b90ff36c300901f4b47d7532ae319
DATA8320-28 <DATA8320-28@DESKTOP-2A20AOL>
2020-05-29 11:57:14 +0900

    복습 02 올립니다.

---
+++ ysg/src/ysg/Arraylist02.java
@@ -0,0 +1,34 @@
+package ysg;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Arraylist02 {
+
+	public static void main(String[] args) {
+				
+				List<String> rList = new ArrayList<>();
+				
+				rList.add("이협건");
+				rList.add("홍길동");
+				rList.add("임꺽정");
+				rList.add("고길동");
+				
+			System.out.println("배열의 크기"+ rList.size());
+			
+			for (int i = 0; i < rList.size(); i++) {
+				System.out.println("["+ i +"]번째 배열 기억공간 : " + rList.get(i));
+			
+			}
+			Iterator<String> it = rList.iterator();
+			while (it.hasNext()) {
+				String name = (String) it.next();
+				
+				System.out.println("이름 : "+ name);
+				 
+			}
+			}
+
+
+	}
